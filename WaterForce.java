262cc9b39f60526db3b714d31344ae9b5cca3487
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-07 20:42:35 +0300

    Create WaterForce.java

---
+++ WaterForce.java
@@ -0,0 +1,15 @@
+class WaterForce extends Unit {
+
+    WaterForce(int health, int damage, int protection, String unitFraction) {
+        super(health, damage, protection, unitFraction);
+    }
+
+    public void attack() {
+    }
+
+    public void defend() {
+    }
+
+    public void explore() {
+    }
+}
