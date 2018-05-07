362cb2c8d8d0cb39bcdb1434eb253368bb002896
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-07 20:41:28 +0300

    Create MermaidFactory.java

---
+++ MermaidFactory.java
@@ -0,0 +1,19 @@
+class MermaidFactory extends Factory {
+
+    public static String fraction = MermaidFactory.class.getTypeName();
+
+    @Override
+    AirForce createAirForce() {
+        return new AirForce(60, 20, 0, fraction);
+    }
+
+    @Override
+    GroundForce createGroundForce() {
+        return new GroundForce(70, 30, 0, fraction);
+    }
+
+    @Override
+    WaterForce createInfantry() {
+        return new WaterForce(80, 40, 0, fraction);
+    }
+}
