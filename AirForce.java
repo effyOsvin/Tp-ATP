0685875d4fd993bbb36a9f68475a0d218773b121
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-07 20:35:31 +0300

    Create AirForce.java

---
+++ AirForce.java
@@ -0,0 +1,12 @@
+class AirForce extends Unit {
+
+    AirForce(int health, int damage, int protection, String unitFraction) {
+        super(health, damage, protection, unitFraction);
+    }
+
+    public void attack() {
+    }
+
+    public void explore() {
+    }
+}
