60afc0d6c0425f41a13f8e1154928558e1adad49
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-07 20:38:54 +0300

    Create GroundForce.java

---
+++ GroundForce.java
@@ -0,0 +1,12 @@
+class GroundForce extends Unit {
+
+    GroundForce(int health, int damage, int protection, String unitFraction) {
+        super(health, damage, protection, unitFraction);
+    }
+
+    public void attack() {
+    }
+
+    public void defend() {
+    }
+}
