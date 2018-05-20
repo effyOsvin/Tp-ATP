229acd5f8551b63cccfd0c4087ac52fe46c94873
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-21 01:39:44 +0300

    Update FairyFactory.java

--- FairyFactory.java
+++ FairyFactory.java
@@ -1,19 +1,31 @@
 class FairyFactory extends Factory {
 
-    public static String fraction = FairyFactory.class.getTypeName();
+    private static final String race = FairyFactory.class.getTypeName();
 
     @Override
     AirForce createAirForce() {
-        return new AirForce(85, 35, 0, fraction);
+        return new AirForce(UnitCharacteristics.Fairy_Air_Health,
+                UnitCharacteristics.Fairy_Air_Damage,
+                UnitCharacteristics.Fairy_Air_Protect,
+                UnitCharacteristics.Fairy_Air_Range,
+                race);
     }
 
     @Override
     GroundForce createGroundForce() {
-        return new GroundForce(65, 15, 0, fraction);
+        return new GroundForce(UnitCharacteristics.Fairy_Ground_Health,
+                UnitCharacteristics.Fairy_Ground_Damage,
+                UnitCharacteristics.Fairy_Ground_Protect,
+                UnitCharacteristics.Fairy_Air_Range,
+                race);
     }
 
     @Override
     WaterForce createWaterForce() {
-        return new WaterForce(75, 25, 0, fraction);
+        return new WaterForce(UnitCharacteristics.Fairy_Water_Health,
+                UnitCharacteristics.Fairy_Water_Damage,
+                UnitCharacteristics.Fairy_Water_Protect,
+                UnitCharacteristics.Fairy_Air_Range,
+                race);
     }
 }
