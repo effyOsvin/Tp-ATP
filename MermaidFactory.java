9faddcb6c39a9950c10af57e3dd9090bf63bfc26
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-21 01:40:49 +0300

    Update MermaidFactory.java

--- MermaidFactory.java
+++ MermaidFactory.java
@@ -1,19 +1,31 @@
 class MermaidFactory extends Factory {
 
-    public static String fraction = MermaidFactory.class.getTypeName();
+    public static String race = MermaidFactory.class.getTypeName();
 
     @Override
     AirForce createAirForce() {
-        return new AirForce(60, 20, 0, fraction);
+        return new AirForce(UnitCharacteristics.Mermaid_Air_Health,
+                UnitCharacteristics.Mermaid_Air_Damage,
+                UnitCharacteristics.Mermaid_Air_Protect,
+                UnitCharacteristics.Mermaid_Air_Range,
+                race);
     }
 
     @Override
     GroundForce createGroundForce() {
-        return new GroundForce(70, 30, 0, fraction);
+        return new GroundForce(UnitCharacteristics.Mermaid_Ground_Health,
+                UnitCharacteristics.Mermaid_Ground_Damage,
+                UnitCharacteristics.Mermaid_Ground_Protect,
+                UnitCharacteristics.Mermaid_Ground_Range,
+                race);
     }
 
     @Override
     WaterForce createWaterForce() {
-        return new WaterForce(80, 40, 0, fraction);
+        return new WaterForce(UnitCharacteristics.Mermaid_Water_Health,
+                UnitCharacteristics.Mermaid_Water_Damage,
+                UnitCharacteristics.Mermaid_Water_Protect,
+                UnitCharacteristics.Mermaid_Water_Range,
+                race);
     }
 }
