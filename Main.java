e54fbd0f659cd240b6686cbb5467db4bf5075bd9
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-21 03:18:00 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        GameWorld world = GameWorld.getGameWorld();
-
-        Client brave = new Client(new MermaidFactory());
-        Client cunning = new Client(new WitchFactory());
-        Client prosperous = new Client(new FairyFactory());
-
-        brave.createWaterForce();
-        cunning.createAirForce();
-        prosperous.createGroundForce();
+        Game game = new Game();
+        game.run();
     }
 }
