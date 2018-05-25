6152c5c3bcb28f5e04dc7dd8b6f02ac2f4273f8a
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-25 11:13:03 +0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Menu {
 
-    private Client createClient(int fractionCode, boolean artificialIntelligence, int difficultyLevel) {
+    private Scanner in;
+
+    public Menu() {
+        in = new Scanner(System.in);
+    }
+
+    private Client createClient(int fractionCode, boolean artificialIntelligence) {
         ArtificialIntelligence client;
         switch (fractionCode) {
             case 1:
@@ -15,7 +23,41 @@ public class Menu {
             default:
                 return null;
         }
+        client.setStrategy(new Strategy());
         return client;
     }
 
+    public Client[] run(){
+
+        System.out.println("Hello, my friend!\n" +
+                "We have a war now.\n" +
+                "Forces are equal..\n" +
+                "You are our last reliable.\n" +
+                "Command the army and win.");
+
+        boolean requestSuccess;
+
+        System.out.println("Choose your race and race your enemy:\n" +
+                "> type '1' for Fairy\n" +
+                "> type '2' for Witch\n" +
+                "> type '3' for Mermaid");
+
+        Client[] clients = new Client[2];
+        
+        for (int i = 0; i < 2; i++) {
+            requestSuccess = false;
+            int fractionCode;
+            do {
+                fractionCode = ConsoleInterfaceUtils.getInput();
+                if (fractionCode > 0 && fractionCode < 4) {
+                    requestSuccess = true;
+                } else {
+                    ConsoleInterfaceUtils.inputMismatchWarning();
+                }
+            } while (!requestSuccess);
+            clients[i] = createClient(fractionCode, i != 0);
+        }
+
+        return clients;
+    }
 }
