a5b48f322fafdcea74bc90e09da1ee87bf5b5906
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-05-25 11:32:14 +0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,7 +1,6 @@
 public class Menu {
 
-
-    public Menu() {    }
+    public Menu() {}
 
     private Client createClient(int fractionCode, boolean artificialIntelligence) {
         ArtificialIntelligence client;
@@ -30,24 +29,23 @@ public class Menu {
                 "You are our last reliable.\n" +
                 "Command the army and win.");
 
-        boolean requestSuccess;
+        boolean requestSuccess = false;
 
         System.out.println("Choose your race and race your enemy:\n" +
                 "> type '1' for Fairy\n" +
-                "> type '2' for Mermaid\n" +
-                "> type '3' for Witch");
+                "> type '2' for Witch\n" +
+                "> type '3' for Mermaid");
 
         Client[] clients = new Client[2];
-
         for (int i = 0; i < 2; i++) {
             requestSuccess = false;
             int fractionCode;
             do {
-                fractionCode = ConsoleInterfaceUtils.getInput();
+                fractionCode = ConsoleInterface.getInput();
                 if (fractionCode > 0 && fractionCode < 4) {
                     requestSuccess = true;
                 } else {
-                    ConsoleInterfaceUtils.inputMismatchWarning();
+                    ConsoleInterface.inputMismatchWarning();
                 }
             } while (!requestSuccess);
             clients[i] = createClient(fractionCode, i != 0);
